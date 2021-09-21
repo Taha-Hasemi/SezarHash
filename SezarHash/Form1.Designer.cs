9d8dc79ca9bc5519e212e64ea687a889ad638d65
kizil-aslan <71307248+kizil-aslan@users.noreply.github.com>
2021-09-21 15:05:46 +0430

    First

--- SezarHash/Form1.Designer.cs
+++ SezarHash/Form1.Designer.cs
@@ -29,13 +29,157 @@ namespace SezarHash
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rchTxtMetin = new System.Windows.Forms.RichTextBox();
+            this.nudSifrelemeSayi = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rchTxtSifre = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSifrelemeSayi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rchTxtMetin
+            // 
+            this.rchTxtMetin.BackColor = System.Drawing.Color.DarkGray;
+            this.rchTxtMetin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rchTxtMetin.ForeColor = System.Drawing.Color.White;
+            this.rchTxtMetin.Location = new System.Drawing.Point(12, 65);
+            this.rchTxtMetin.Name = "rchTxtMetin";
+            this.rchTxtMetin.Size = new System.Drawing.Size(412, 107);
+            this.rchTxtMetin.TabIndex = 0;
+            this.rchTxtMetin.Text = "";
+            // 
+            // nudSifrelemeSayi
+            // 
+            this.nudSifrelemeSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudSifrelemeSayi.Location = new System.Drawing.Point(155, 22);
+            this.nudSifrelemeSayi.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.nudSifrelemeSayi.Name = "nudSifrelemeSayi";
+            this.nudSifrelemeSayi.Size = new System.Drawing.Size(139, 24);
+            this.nudSifrelemeSayi.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DimGray;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(98, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sifre";
+            // 
+            // rchTxtSifre
+            // 
+            this.rchTxtSifre.BackColor = System.Drawing.Color.DarkGray;
+            this.rchTxtSifre.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rchTxtSifre.ForeColor = System.Drawing.Color.White;
+            this.rchTxtSifre.Location = new System.Drawing.Point(12, 223);
+            this.rchTxtSifre.Name = "rchTxtSifre";
+            this.rchTxtSifre.Size = new System.Drawing.Size(412, 107);
+            this.rchTxtSifre.TabIndex = 3;
+            this.rchTxtSifre.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(103, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Şıfrele";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(103, 336);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 39);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Çöz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(7, 408);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(417, 41);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Uygulama Kızıl Aslan tarafından tasarlandı";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(222, 178);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 39);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Temizle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(222, 336);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 39);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Temizle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(439, 484);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rchTxtSifre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudSifrelemeSayi);
+            this.Controls.Add(this.rchTxtMetin);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Sezar Şifreleme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudSifrelemeSayi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox rchTxtMetin;
+        private System.Windows.Forms.NumericUpDown nudSifrelemeSayi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rchTxtSifre;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- SezarHash/Form1.cs
+++ SezarHash/Form1.cs
@@ -16,5 +16,60 @@ namespace SezarHash
         {
             InitializeComponent();
         }
+        string metin = "";
+        int sifre = 0;
+        string sifrelenmisMetin = "";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sifrelenmisMetin = "";
+            try
+            {
+                sifre = Convert.ToInt32(nudSifrelemeSayi.Value);
+                metin = rchTxtMetin.Text;
+                char[] karakterler = metin.ToCharArray();
+                foreach (var element in karakterler)
+                {
+                    sifrelenmisMetin += Convert.ToChar(element + sifre).ToString();
+                }
+                rchTxtSifre.Text = sifrelenmisMetin;
+            }
+            catch (Exception aciklama)
+            {
+                MessageBox.Show(aciklama.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            metin = "";
+            try
+            {
+                sifre = Convert.ToInt32(nudSifrelemeSayi.Value);
+                sifrelenmisMetin = rchTxtSifre.Text;
+                char[] karakterler = sifrelenmisMetin.ToCharArray();
+                foreach (var element in karakterler)
+                {
+                    metin += Convert.ToChar(element - sifre).ToString();
+                }
+                rchTxtMetin.Text = metin;
+            }
+            catch (Exception aciklama)
+            {
+                MessageBox.Show(aciklama.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            rchTxtMetin.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            rchTxtSifre.Clear();
+        }
     }
 }
